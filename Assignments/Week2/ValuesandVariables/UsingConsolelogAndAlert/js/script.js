290c58afb9d725654f0be3b540adcfa4589fc1e7
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-03 16:44:09 -0400

    Adding New Files to Repo

---
+++ Assignments/Week2/ValuesandVariables/UsingConsolelogAndAlert/js/script.js
@@ -0,0 +1,12 @@
+//JavaScript
+
+var yearBorn = 1995;
+
+console.log(yearBorn); // Logs the yearBoron Variable
+console.log("Hello World"); // Prints a string
+
+/*alert("Hello World"); // Alert 
+alert(yearBorn); // Alerts with yearBorn Variable Value
+*/
+
+
