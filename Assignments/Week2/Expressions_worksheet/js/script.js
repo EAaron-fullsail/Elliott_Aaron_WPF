061503207a398fe320d94e36bd8ecdebd0afe39d
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-08 22:03:55 -0400

    Added files for this assignments

--- Assignments/Week2/Expressions_worksheet/js/script.js
+++ Assignments/Week2/Expressions_worksheet/js/script.js
@@ -1,1 +1,12 @@
-alert("This was for an Assignment");
+/*
+	Aaron Elliott
+	WPF 1409 Section 02
+	Expression Worksheet
+	9-8-2014
+*/
+
+var a = "Hello";
+
+var problemOne = getElementById("1");
+
+problemOne.update(a);
