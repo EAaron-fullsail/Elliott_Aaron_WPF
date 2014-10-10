65bda3fc7276f6d3defa19b5a98ebfaee7a60136
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-09 22:46:49 -0400

    Updating script.js

--- Assignments/Week2/Expressions_Personal/js/script.js
+++ Assignments/Week2/Expressions_Personal/js/script.js
@@ -18,7 +18,7 @@ var howLong = parseInt(prompt("How many years before your would like your loans 
 //Get input from user 
 var howMuch = parseInt(prompt("how much do you expect to to pay each month?"));
 
-
+// Array to hold all of the inputs from user and one input used throughout the rest of the code
 var inputs = [];
 
 inputs[0] = amountOwed;
@@ -26,25 +26,28 @@ inputs[1] = howLong;
 inputs[2] = howMuch;
 inputs[3] = 12;
 
-// how much they currently owe / (12 months * expected monthly payments )= total years 
+// Calculate how much they currently owe / (12 months * expected monthly payments )= total years 
 var actualAmount = amountOwed / (12 * inputs[2]);
 
+// Calculate payments if they do not make their goal
 var prefPayments = inputs[0] / ( inputs[3] * inputs[1] );
 
+// Calculate payments if they do not make their goal
 var onTrackPayments = inputs[0] / ((inputs[1]*inputs[3])*inputs[2])
 
+// check to see if the student will make his or her repayment goal
 if( inputs[1] != actualAmount) {
+	//If they are not going to make it
+	console.log("Uh Oh! You wont be able to repay you loans in that amount of time with such low monthly Payments");// Message the user
 
-	console.log("Uh Oh! You wont be able to repay you loans in that amount of time with such low monthly Payments");
-	console.log("To repay $" + inputs[0] + " in " + inputs[1] + " years you will need to make monthly payments of $" + prefPayments);
+	console.log("To repay $" + inputs[0] + " in " + inputs[1] + " years you will need to make monthly payments of $" + prefPayments);// Update the user
 
 }else{
+	// If the student is going to make it
+	console.log("Great! You're Right one track!"); // Message the user
 
-	console.log("Great! You're Right one track!");
-	console.log("You can repay $" + inputs[0] + " in " + onTrackPayments + " years making monthly payments of $" + inputs[2]);
+	console.log("You can repay $" + inputs[0] + " in " + onTrackPayments + " years making monthly payments of $" + inputs[2]); // Update the user
 }
-
-console.log(actualAmount)
 
 
 
