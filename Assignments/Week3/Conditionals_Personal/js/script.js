1dffb5d04f771f2b8be73af80c96bab61ac6ee4a
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-16 23:43:06 -0400

    Cleaning up

--- Assignments/Week3/Conditionals_Personal/js/script.js
+++ Assignments/Week3/Conditionals_Personal/js/script.js
@@ -22,43 +22,42 @@ if (first == "no"){ // if the user says no
 	
 	console.log("Let's see how fast you shoot!"); // let the user know what's going on
 	
-	console.log("alternate tapping your index and middle finger as many times as you can for 15seconds");
+	console.log("alternate tapping your index and middle finger as many times as you can for 15seconds"); // tell the user what to do 
 	
-	var taps = parseInt(prompt("How many taps did you get? \n just type the number please"));
+	var taps = parseInt(prompt("How many taps did you get? \n just type the number please")); // get their input 
 
-	var tapTypes = [];
+	var tapTypes = []; // array for comp types 
 
-	tapTypes[0] = 10;
+	tapTypes[0] = 10; // array element
 	
-	tapTypes[1] = 30;
+	tapTypes[1] = 30;// array element
 	
-	tapTypes[2] = 50;
+	tapTypes[2] = 50;// array element
 
 
-	if(taps <= tapTypes[0]){
+	if(taps <= tapTypes[0]){ // if they tap less than 10 times 
 	
-		console.log(taps + " bps is pretty good! Keep practicing though");
+		console.log(taps + " bps is pretty good! Keep practicing though"); // print this mmessage 
 	
-	}else if(taps > tapTypes[0] && taps < tapTypes[1]){
+	}else if(taps > tapTypes[0] && taps < tapTypes[1]){ // if they tap more than 10 and less than 30 
 	
-		console.log(taps + " bps is pretty good! Keep practicing though");
+		console.log(taps + " bps is pretty good! Keep practicing though"); // print this message 
+
+	}else if(taps > tapTypes[1] && taps < tapTypes[2]){// if they tap more than 30 and less that 50 
+	
+		console.log(taps + " bps Is Really good! Keep it up!");// print this message 
+	
+	}else if(taps >= tapTypes[2] ){ // if they tap more than 50 
+	
+		console.log(" You're off the scale you're so good"); // print this message 
+	
 	}
-	
-	else if(taps > tapTypes[1] && taps < tapTypes[2]){
-	
-		console.log(taps + " bps Is Really good! Keep it up!");
-	
-	}else if(taps >= tapTypes[2] ){
-	
-		console.log(" You're off the scale you're so good");
-	
-	}
 
-	var bps = taps * simi / 15; 
+	var bps = taps * simi / 15; // calc balls per second 
 	
-	var ramping = ramping * taps / 15; 
+	var ramping = ramping * taps / 15; // calc balls per second  with ramping 
 	
-	console.log("you can shoot "+ bps +" ball(s) a second without ramping, and " + ramping + " balls per second with ramping");
+	console.log("you can shoot "+ bps +" ball(s) a second without ramping, and " + ramping + " balls per second with ramping"); // print results 
 	
 
 
