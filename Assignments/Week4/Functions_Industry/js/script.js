9eb1f5c9a1d464adf11c8459a0ad2c4efbb3454c
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-23 18:58:47 -0400

    Finished the first one started work on the second one

--- Assignments/Week4/Functions_Industry/js/script.js
+++ Assignments/Week4/Functions_Industry/js/script.js
@@ -1,6 +1,56 @@
-var b = true, a = 10;
+	//find the alloted space for a user
+	(function(){ // anon function d      
 
-a > 5 ? b = true : b = false;
+		var hasCalled
+		var hasFinished;
+
+		//A ternary
+		
+
+		var images = [];
+
+		images[0] = {"n": "Squirell", "url": "images/", "idn": "1"} 
+
+		images[1] = {"n": "Banner", "url": "images/", "idn": "2"} 
+
+		images[2] = {"n": "Bubble", "url": "images/", "idn": "3"} 
+
+		var totalImages = images.length,
+		totalAllowedImages = 32,
+		message = "You have used " + Math.round(totalImages / totalAllowedImages * 10) /10 + " of your alloted space",
+		i,
+		getLoc = function(){
 
 
-console.log(b);
+			
+			for(i=0; i<=totalImages; i++){
+				
+				//console.log("Getting new image");
+				//console.log("Image id: " +images[i].idn+ "  " + "Name : " +images[i].n+ "  " + " location: " + images[i].url );
+				i == totalImages ? hasFinished = false : hasFinished = true;
+				return hasFinished
+				}
+		}
+
+		
+		//a function with three params
+		function listImages(list, called, finished){
+			hasCalled = true;
+			console.log("Getting Data");
+
+			if(i<totalImages){
+			//one logical operator
+				getLoc();
+
+			}else if(hasFinished = true){
+					console.log(message);
+				}
+
+
+			}
+
+
+	listImages(images, hasCalled, hasFinished);
+
+
+	})();
