d292badea875c0940c15a902454efb2a79263a31
Aaron Elliott <aaronelliott@Aarons-MacBook-Pro.local>
2014-10-23 19:30:17 -0400

    Finished second part of assignment

--- Assignments/Week4/Functions_Industry/js/script.js
+++ Assignments/Week4/Functions_Industry/js/script.js
@@ -9,45 +9,44 @@
 
 		var images = [];
 
-		images[0] = {"n": "Squirell", "url": "images/", "idn": "1"} 
+		images[0] = {"n": "dog", "url": "images/Dog.jpg", "idn": "1"} 
 
-		images[1] = {"n": "Banner", "url": "images/", "idn": "2"} 
+		images[1] = {"n": "Banner", "url": "images/Banner.jph", "idn": "2"} 
 
-		images[2] = {"n": "Bubble", "url": "images/", "idn": "3"} 
+		images[2] = {"n": "Bubble", "url": "images/Buble.jpg", "idn": "3"} 
 
 		var totalImages = images.length,
 		totalAllowedImages = 32,
 		message = "You have used " + Math.round(totalImages / totalAllowedImages * 10) /10 + " of your alloted space",
 		i,
-		getLoc = function(){
-
-
-			
-			for(i=0; i<=totalImages; i++){
-				
-				//console.log("Getting new image");
-				//console.log("Image id: " +images[i].idn+ "  " + "Name : " +images[i].n+ "  " + " location: " + images[i].url );
-				i == totalImages ? hasFinished = false : hasFinished = true;
-				return hasFinished
-				}
+		getLoc = function(i){
+			i == totalImages ? hasFinished = false : hasFinished = true;
+			return hasFinished
 		}
 
-		
 		//a function with three params
 		function listImages(list, called, finished){
 			hasCalled = true;
-			console.log("Getting Data");
-
-			if(i<totalImages){
+			console.log("Getting List Items")
 			//one logical operator
-				getLoc();
+				for(i=0; i<totalImages; i++){
+					getLoc(i);
+					console.log("Getting new image");
+					console.log("Image id: " +images[i].idn+ "  " + "Name : " +images[i].n+ "  " + " location: " + images[i].url );
 
-			}else if(hasFinished = true){
-					console.log(message);
 				}
-
-
-			}
+				
+			if(i > totalImages){ //does nothing
+			
+			//do nothing
+			
+			}else if(hasFinished = true){
+			
+					console.log(message);
+			
+				}
+			
+			}//end listh Images
 
 
 	listImages(images, hasCalled, hasFinished);
